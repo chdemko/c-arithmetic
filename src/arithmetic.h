9cb01a3820047565f05c3601357bb5ee0582ead9
Christophe Demko <chdemko@gmail.com>
2017-03-30 08:46:39 +0200

    First commit

---
+++ src/arithmetic.h
@@ -0,0 +1,7 @@
+#ifndef _ARITHMETIC_H_
+#define _ARITHMETIC_H_
+
+extern unsigned int arithmetic_gcd(unsigned int a, unsigned int b);
+
+#endif				/* _ARITHMETIC_H_ */
+
---
+++ test/test-gcd.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#include <assert.h>
+
+#include "../src/arithmetic.h"
+
+int main(void)
+{
+	assert(arithmetic_gcd(32, 56) == 8);
+	assert(arithmetic_gcd(101, 23) == 1);
+	assert(arithmetic_gcd(101, 0) == 101);
+	assert(arithmetic_gcd(0, 101) == 101);
+	assert(arithmetic_gcd(0, 0) == 0);
+	return EXIT_SUCCESS;
+}
+
